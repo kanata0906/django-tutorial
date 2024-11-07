64cf4443eec595c621370ddc7634d25220a7f843
kanata <shiny.purine@gmail.com>
2024-11-07 12:03:16 +0900

    はじめての Django アプリ作成、その７

--- polls/admin.py
+++ polls/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
